ea05948e3014bbce4c0e4aa29c3e407e4859cad1
lktrgl <32882989+lktrgl@users.noreply.github.com>
2019-12-24 00:54:17 +0200

    d

--- Funnel.m
+++ Funnel.m
@@ -1,20 +1,22 @@
 
-G = 1000000;
-M = 1000000;
+G = 1;
+M = 1;
 global K = -G*M;
 global Eps=0.00001;
 
-function y1=Fx(x,y)
+function fx=Fx(x,y)
   global K
   global Eps
-  y1=0;##K*y./(x.^2+y.^2+Eps);
-endfunction
-function y1=Fy(x,y)
-  global K
-  global Eps
-  y1=0;##K*x./(x.^2+y.^2+Eps);
+  tmp=x.^2+y.^2+Eps;
+  fx=K*y./(sqrt(tmp).*tmp);
 endfunction
 
+function fy=Fy(x,y)
+  global K
+  global Eps
+  tmp=x.^2+y.^2+Eps;
+  fy=K*x./(sqrt(tmp).*tmp);
+endfunction
 
 ##ty=tx=linspace(-5,5,50)';
 ##[xx, yy] = meshgrid (tx, ty);
@@ -22,15 +24,16 @@ endfunction
 ##tz = sqrt (Fx (K,xx,yy).^2+Fy (K,xx,yy).^2);
 ##mesh (tx, ty, tz);
 
-
 function xdot = pend(x,t)
   xdot(1) = x(2); 
   xdot(2) = Fx(x(1),x(3));
   xdot(3) = x(4); 
   xdot(4) = Fy(x(1),x(3));
 end
-  sol=lsode( "pend",[10, -0.1,-10,-0.1], t = linspace(0,10, 10000));
-  plot( t, sol,"-@")
+
+t = linspace(0,7, 1000);
+sol=lsode( "pend",[10, -1, 10, -1], t);
+##plot( t, sol,"-@")
 ##plot( t,[sol(:,1)';sol(:,3)'],"-@")
-##  plot( sol(:,1)',sol(:,2)',"-@")
+plot( sol(:,1)',sol(:,3)',"-@")
 
