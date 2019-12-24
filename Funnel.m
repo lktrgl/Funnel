461a452df3c76f942f01e112d6c2462e1d4b7edf
lktrgl <32882989+lktrgl@users.noreply.github.com>
2019-12-25 01:58:47 +0200

    the Funel movement is drown well )

--- Funnel.m
+++ Funnel.m
@@ -1,39 +1,82 @@
+clear all
 
-G = 1;
-M = 1;
-global K = -G*M;
-global Eps=0.00001;
+#-------------------------------------
+
+T0 = 0;
+Tmax = 30;
+
+X0 = 1;
+Vx0 = 0;
+Y0 = 0;
+Vy0 = 1;
+
+#-------------------------------------
+
+Npoints = 10000;
+
+#-------------------------------------
+
+global G = 1;
+global M = 1;
+global Eps = 0.01;
+global Mju = 0.7;
+global K = G * M;
+global Kz = G * M;
+
+#-------------------------------------
+
+function fr=getR(x,y)
+  global Eps;
+  fr = sqrt(x.^2 + y.^2 + Eps);
+endfunction
+
+#-------------------------------------
 
 function fx=Fx(x,y)
-  global K
-  global Eps
-  tmp=x.^2+y.^2+Eps;
-  fx=K*y./(sqrt(tmp).*tmp);
+  global K;
+  fx = -K * x ./ getR(x,y);
 endfunction
+
+#-------------------------------------
 
 function fy=Fy(x,y)
-  global K
-  global Eps
-  tmp=x.^2+y.^2+Eps;
-  fy=K*x./(sqrt(tmp).*tmp);
+  global K;
+  fy = -K * y ./ getR(x,y);
 endfunction
 
-##ty=tx=linspace(-5,5,50)';
-##[xx, yy] = meshgrid (tx, ty);
-##
-##tz = sqrt (Fx (K,xx,yy).^2+Fy (K,xx,yy).^2);
-##mesh (tx, ty, tz);
+#-------------------------------------
 
-function xdot = pend(x,t)
+function fz=Fz(x,y)
+  global Kz;
+  fz = Kz * getR(x,y).^2;
+endfunction
+
+#-------------------------------------
+
+function xdot = insideTheFunelMovement(x,t)
+  global Mju;
   xdot(1) = x(2); 
-  xdot(2) = Fx(x(1),x(3));
+  xdot(2) = -x(1) -Mju * x(2) + Fx(x(1),x(3));
   xdot(3) = x(4); 
-  xdot(4) = Fy(x(1),x(3));
+  xdot(4) = -x(3) -Mju * x(4) + Fy(x(1),x(3));
 end
 
-t = linspace(0,7, 1000);
-sol=lsode( "pend",[10, -1, 10, -1], t);
-##plot( t, sol,"-@")
-##plot( t,[sol(:,1)';sol(:,3)'],"-@")
-plot( sol(:,1)',sol(:,3)',"-@")
+#-------------------------------------
+
+#
+#  Main
+#
+
+#-------------------------------------
+
+t = linspace(T0,Tmax, Npoints);
+sol=lsode( "insideTheFunelMovement",[X0, Vx0, Y0, Vy0], t);
+
+solZ=Fz(sol(:,1),sol(:,3));
+plot3( sol(:,1)',sol(:,3)',solZ',"-@")
+
+xlabel ("ось X");
+ylabel ("ось Y");
+zlabel ("ось Z");
+title ("3-D график движения в воронке");
 
